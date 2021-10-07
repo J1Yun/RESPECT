af49e1022e6bdb16df84a8f6cd3d9137276fd03e
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-07 18:29:12 +0900

    GithubAPI

---
+++ server/src/routes/githubapi.js
@@ -0,0 +1,16 @@
+const { appendFile } = require('fs');
+const GitHub = require('github-api'); // npm install github-api
+const noAuth = new GitHub(); // noAuth 방식 -> token, noAuth, 
+
+// 사용자 레포지토리 리스트 조회
+exports.getUserRepositoryList = function(req, res){
+    var repositoryList = noAuth.getUser(`${req.params.userId}`);
+    
+    repositoryList.listRepos(function(err,repos){
+        repos.forEach((repo) => {
+            console.log(`"${repo.name} " : "${repo.owner.login}"`)
+            repoInfo.repository.name.push(repo.name);
+        })
+        res.json(repos);
+    })
+}
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('./user.controller');
+const githubapi = require('./githubapi');
 
 router.get('/login', userController.output.login);
+router.get('/userRepository/:userId', githubapi.getUserRepositoryList);
 
 module.exports = router;
