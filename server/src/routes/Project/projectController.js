497072cfed98949c10156b0fc37c7413ae28be87
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-19 01:30:34 +0900

    Add: Show All Project

--- server/src/routes/Project/projectController.js
+++ server/src/routes/Project/projectController.js
@@ -4,6 +4,6 @@ const { response } = require('../../config/baseResponseStatus');
 
 exports.projectList = async function (req, res) {
   const userId = req.params.userId;
-  const showProject = await ProjectService.getProjectList(userId);
-  res.json(showProject);
+  const projectResult = await ProjectService.getProjectList(userId);
+  res.json(projectResult);
 };
