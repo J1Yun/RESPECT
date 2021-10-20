778a44c1a15e222d4464dafbb0eb8269d5425440
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-20 23:42:33 +0900

    Add: ProjectComments
    Get /comment/:projectId
    Project Comments

--- server/src/routes/Comment/commentController.js
+++ server/src/routes/Comment/commentController.js
@@ -2,9 +2,13 @@ const CommentService = require('./commentService');
 const baseResponse = require('../../config/baseResponseStatus');
 const { response } = require('../../config/baseResponseStatus');
 
-exports.projectComment = async function (req, res) {
-  const projectId = req.params.projectId;
-  const Comments = await CommentService.getAllComments(projectId);
-  console.log(Comments);
-  res.json(Comments);
-};
+class Comment {
+  constructor() {}
+  projectComment = async function (req, res) {
+    const projectId = req.params.projectId;
+    const Comments = await CommentService.getAllComments(projectId);
+    console.log(Comments);
+    res.json(Comments);
+  };
+}
+module.exports = new Comment();
