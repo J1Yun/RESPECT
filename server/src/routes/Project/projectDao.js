2426ce4c5322d28f9606adae13935c1f3cd8f1a1
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-29 01:31:22 +0900

    Add: retrieve Project

--- server/src/routes/Project/projectDao.js
+++ server/src/routes/Project/projectDao.js
@@ -10,6 +10,21 @@ async function showProjectList(connection, userId) {
 }
 async function retrieveProject(connection, userId, projectId) {
   const getProjectInfo = `
+       select u.nickname   as userNickname,
+       u.name       as userName,
+       u.imageUrl   as userBackgroundImage,
+       p.name       as projectName,
+       p.role       as projectRole,
+       p.about      as projectAbout,
+       p.start      as projectStart,
+       p.end        as projectEnd,
+       p.readMe     as readMe,
+       p.stack      as teckStack,
+       p.projectUrl as projectUrl,
+       p.imageUrl   as projectImageUrl
+    from Project p
+         left join User u on p.userId = u.id
+    where u.id = ${userId} and p.id = ${projectId};
   `;
   const [getProjectByUserIdProjectId] = await connection.query(getProjectInfo, userId, projectId);
   return getProjectByUserIdProjectId;
