249c25f71532d79c106c3dc3fcbd710ce3dde197
J1yun <cjy5025@naver.com>
2021-11-25 22:09:02 +0900

    explanatory notes

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+// import React from 'react';
+// import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import LoginPage from 'pages/LoginPage/LoginPage';
-import RegisterPage from 'pages/RegisterPage/RegisterPage';
-import AroundPage from 'pages/AroundPage/AroundPage';
-import PoolPage from 'pages/PoolPage/PoolPage';
-import PortfolioPage from 'pages/PortfolioPage/PortfolioPage';
-import CallbackPage from 'pages/GithubLogin/CallbackPage';
-import RepositoryPage from 'pages/ProjectPage/RepositoryPage';
+// import LoginPage from 'pages/LoginPage/LoginPage';
+// import RegisterPage from 'pages/RegisterPage/RegisterPage';
+// import AroundPage from 'pages/AroundPage/AroundPage';
+// import PoolPage from 'pages/PoolPage/PoolPage';
+// import PortfolioPage from 'pages/PortfolioPage/PortfolioPage';
+// import CallbackPage from 'pages/GithubLogin/CallbackPage';
+// import RepositoryPage from 'pages/ProjectPage/RepositoryPage';
 
-const AppRouter = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/login">
-          <LoginPage />
-        </Route>
-        <Route exact path="/register">
-          <RegisterPage />
-        </Route>
-        <Route exact path="/around">
-          <AroundPage />
-        </Route>
-        <Route exact path="/pool">
-          <PoolPage />
-        </Route>
-        <Route exact path="/user/:nickname">
-          <PortfolioPage />
-        </Route>
-        {/* <Route exact path="/portfolio">
-          <CallbackPage />
-        </Route> */}
-        <Route exact path="/repository">
-          <RepositoryPage />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  );
-};
+// const AppRouter = () => {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/login">
+//           <LoginPage />
+//         </Route>
+//         <Route exact path="/register">
+//           <RegisterPage />
+//         </Route>
+//         <Route exact path="/around">
+//           <AroundPage />
+//         </Route>
+//         <Route exact path="/pool">
+//           <PoolPage />
+//         </Route>
+//         <Route exact path="/user/:nickname">
+//           <PortfolioPage />
+//         </Route>
+//         {/* <Route exact path="/portfolio">
+//           <CallbackPage />
+//         </Route> */}
+//         <Route exact path="/repository">
+//           <RepositoryPage />
+//         </Route>
+//         <Redirect to="/" />
+//       </Switch>
+//     </Router>
+//   );
+// };
 
-export default AppRouter;
+// export default AppRouter;
