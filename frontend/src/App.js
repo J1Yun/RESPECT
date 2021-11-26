7f824b59357504ff9ae0272492b3648124ead7d6
Yun Jun Sung <wnstjd9701@naver.com>
2021-11-26 17:46:02 +0900

    err

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import MenuBar from "pages/MenuBar/MenuBar";
-import AppRouter from "./AppRouter";
-import GithubLoginPage from "pages/GithubLogin/GithubLoginPage";
-import "./App.css";
+import MenuBar from 'pages/MenuBar/MenuBar';
+import AppRouter from './AppRouter';
+import GithubLoginPage from 'pages/GithubLogin/GithubLoginPage';
+import './App.css';
 function App() {
   return (
     <>
       <MenuBar />
-      <AppRouter />
+      {/* <AppRouter /> */}
       {/* <GithubLoginPage /> */}
     </>
   );
