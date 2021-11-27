eb5a3d45a1339acd0807b59960cbbdfe093ef001
Kim Chae Eun <kce3615@naver.com>
2021-11-27 21:49:27 +0900

    add project page components

--- frontend/src/pages/ProjectPage/ProjectPage.js
+++ frontend/src/pages/ProjectPage/ProjectPage.js
@@ -1,41 +1,23 @@
-import "pages/ProjectPage/ProjectPage.css";
-import { AiFillGithub } from "react-icons/ai";
-import { BsBookmark } from "react-icons/bs";
+import "./styles/ProjectPage.css";
+import ProjectHeader from "./components/ProjectHeader";
+import ProjectOutline from "./components/ProjectOutline";
+
+import ReactMarkdown from "react-markdown";
+import ProjectComments from "./components/ProjectComments";
+
+const markdown = "# hello";
+
 const ProjectPage = () => {
   return (
     <div>
       <div className="project-container">
-        <div className="project-header">
-          <span className="project-header-category">Project</span>
-          <button className="project-header-github">
-            <AiFillGithub color="white" size="20" />
-            <span
-              style={{
-                position: "relative",
-                left: "2px",
-                top: "-4px",
-              }}
-            >
-              Open with Github
-            </span>
-          </button>
-          <button className="project-header-bookmark">
-            <BsBookmark size="25" />
-          </button>
-          <div className="project-header-title">To do list 서비스</div>
-          <div className="project-header-profile">
-            <img
-              width="35"
-              height="35"
-              src="https://1tb.favim.com/preview/7/773/7735/77357/7735766.jpg"
-            />
-            <span>김채은</span>
-            <span>@chchaeun</span>
-          </div>
+        <ProjectHeader />
+        <ProjectOutline />
+        <div className="project-readme-container">
+          <div className="project-readme-title">README</div>
+          <ReactMarkdown className="project-readme" children={markdown} />
         </div>
-        <div className="project-outline-container"></div>
-        <div className="project-readme"></div>
-        <div className="project-comments-container"></div>
+        <ProjectComments />
       </div>
     </div>
   );
---
+++ frontend/src/pages/ProjectPage/components/ProjectOutline.js
@@ -0,0 +1,28 @@
+import "../styles/ProjectOutline.css";
+
+const ProjectOutline = () => {
+  return (
+    <div className="project-outline-container">
+      <img src="https://i.pinimg.com/originals/32/aa/7f/32aa7f743012b4d6f6797489c86ee352.jpg" />
+      <div>
+        <div>
+          <span className="project-outline-titles">Outline</span>
+          <span>Chrome App 'Momentum' Clone coding</span>
+        </div>
+        <div>
+          <span className="project-outline-titles">Role</span>
+          <span>프론트엔드 개발</span>
+        </div>
+        <div>
+          <span className="project-outline-titles">Period</span>
+          <span>2021. 07. ~ 2021. 08.</span>
+        </div>
+        <div>
+          <span className="project-outline-titles">Tech Stack</span>
+          <span>스택 불러오기</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProjectOutline;
