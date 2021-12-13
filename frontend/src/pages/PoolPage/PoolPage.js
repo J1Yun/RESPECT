4591fab073a14ad03aca7a5abdf935765fb79ac3
Kim Chae Eun <kce3615@naver.com>
2021-12-13 14:37:22 +0900

    add pool page

--- frontend/src/pages/PoolPage/PoolPage.js
+++ frontend/src/pages/PoolPage/PoolPage.js
@@ -1,5 +1,30 @@
+import PoolCard from "./components/PoolCard";
+import "./styles/PoolPage.css";
 const PoolPage = () => {
-  return <div></div>;
+  const poolContainer = {
+    position: "absolute",
+    width: "900px",
+    left: "180px",
+    background: "#FFFFFF",
+    marginTop: "75px",
+    zIndex: "-2",
+  };
+  const poolCardContainer = {
+    position: "relative",
+    width: "750px",
+    left: "50%",
+    transform: "translateX(-50%)",
+    zIndex: "-1",
+  };
+
+  return (
+    <div style={poolContainer}>
+      <div style={poolCardContainer}>
+        <div className="pool-header"></div>
+        <PoolCard />
+      </div>
+    </div>
+  );
 };
 
 export default PoolPage;
--- frontend/src/pages/PortfolioPage/components/Profile/ProfileImg.js
+++ frontend/src/pages/PortfolioPage/components/Profile/ProfileImg.js
@@ -1,10 +1,8 @@
-import 'pages/PortfolioPage/styles/Profile/ProfileImg.css'
+import "pages/PortfolioPage/styles/Profile/Profile.css";
 
-
-const ProfileImg = () =>{
-    const img_url = "https://i.pinimg.com/474x/cf/1d/e8/cf1de8648cdfd85ef5ae35061bc61f0b.jpg"
-    return(
-        <img className="profile-img" src={img_url}/>
-    )
-}
+const ProfileImg = () => {
+  const img_url =
+    "https://i.pinimg.com/474x/cf/1d/e8/cf1de8648cdfd85ef5ae35061bc61f0b.jpg";
+  return <img className="profile-img" src={img_url} />;
+};
 export default ProfileImg;
