04803a340f568170f4f59a27ed9125d2ff8a365c
Yun Jun Sung <wnstjd9701@naver.com>
2021-12-21 15:43:28 +0900

    2021-12-21

--- frontend/src/pages/PortfolioPage/containers/Projects.js
+++ frontend/src/pages/PortfolioPage/containers/Projects.js
@@ -1,19 +1,20 @@
-import "pages/PortfolioPage/styles/Projects.css";
-import { useState, useEffect, useRef } from "react";
-import ProjectAdd from "../components/Project/ProjectAdd";
+import axios from 'axios';
+import 'pages/PortfolioPage/styles/Projects.css';
+import { useState, useEffect, useRef } from 'react';
+import ProjectAdd from '../components/Project/ProjectAdd';
 
 const projects = [
   {
-    title: "To do list Service",
-    img: "https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E",
+    title: 'To do list Service',
+    img: 'https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E',
   },
   {
-    title: "방명록 서비스",
-    img: "https://t1.daumcdn.net/cfile/blog/2455914A56ADB1E315",
+    title: '방명록 서비스',
+    img: 'https://t1.daumcdn.net/cfile/blog/2455914A56ADB1E315',
   },
   {
-    title: "영화 순위 서비스",
-    img: "https://t1.daumcdn.net/tistoryfile/fs15/20_tistory_2009_02_24_20_58_49a3e0c987e78?x-content-disposition=inline",
+    title: '영화 순위 서비스',
+    img: 'https://t1.daumcdn.net/tistoryfile/fs15/20_tistory_2009_02_24_20_58_49a3e0c987e78?x-content-disposition=inline',
   },
 ];
 
@@ -21,7 +22,7 @@ const Projects = () => {
   const [add, setAdd] = useState(true);
 
   const onAddClick = () => {
-    setAdd(true);
+    setAdd(false);
   };
   const onAddCloseClick = () => {
     setAdd(false);
@@ -51,12 +52,7 @@ const Projects = () => {
           {projects &&
             projects.map((item) => (
               <div className="project-box">
-                <img
-                  width="250"
-                  height="170"
-                  style={{ objectFit: "cover" }}
-                  src={item.img}
-                />
+                <img width="250" height="170" style={{ objectFit: 'cover' }} src={item.img} />
                 <div>{item.title}</div>
               </div>
             ))}
